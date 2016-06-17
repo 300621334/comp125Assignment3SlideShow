443308aa5dee985981477f38e5c6affaa6836ca0
shafiq <shafiqu@my.centennialcollege.ca>
2016-06-17 18:46:34 -0400

    Slide Show comp125 Assignment3 As it is uploaded to server

---
+++ test.js
@@ -0,0 +1,64 @@
+﻿//window.addEventListener("load", rotatePics, false)
+
+var btn = document.getElementById("btn");
+var btnPause = document.getElementById("pause");
+var btnForward = document.getElementById("forward");
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+btn.addEventListener("click", rotatePics, false);
+btn.addEventListener("click", setRotation, false);
+btnPause.addEventListener("click", pause, false);
+btnForward.addEventListener("click", rotatePics, false);
+
+//btn.addEventListener("click", addFav, false);
+var counterZoom = 0;
+
+var anchor = document.getElementById("top");
+anchor.addEventListener("click", newWindow, false);
+var newWindow;
+var timer; // = setInterval(rotatePics, 3000);
+function rotatePics()
+{
+    var images = document.getElementsByTagName('img');
+    for (var i = 0; i < 9; i++) {
+        if (array[i] == 9)
+        { array[i] = 1; }
+        else
+        { array[i]++; }
+
+
+        images[i].src = array[i] + ".jpg";
+        //anchor.href = array[6] + ".jpg";
+        }
+}
+function setRotation()
+{
+    timer = setInterval(rotatePics, 3000);
+    btn.removeEventListener("click", setRotation, false);
+
+}
+function newWindow()
+{
+    var rightMargin = ((screen.width - 1075) / 2);
+    var topMargin = ((screen.height - 900) / 2);
+    var dimentions = "width=1075, height=900, left="+rightMargin+", top="+topMargin;
+    //event.preventDefault();
+    newWindow = window.open("zoom.html", "", dimentions);
+    //e out dimentions it opens in new "tab". but e dimentions it opes in new "window". or create another page to act as popup window.
+    newWindow.focus();
+    
+    clearInterval(timer);
+}
+
+
+function pause()
+{
+    clearInterval(timer);
+    btn.addEventListener("click", setRotation, false);
+}
+
+function removeFav1(){fav1.innerHTML = "";}
+function removeFav2(){fav2.innerHTML = "";}
+function removeFav3(){fav3.innerHTML = "";}
+function removeFav4(){fav4.innerHTML = "";}
+
+
---
+++ zoom.js
@@ -0,0 +1,39 @@
+﻿var arrayZoom = window.opener.array;
+var imageToOpen = arrayZoom[6] + ".jpg"
+document.getElementsByTagName("img")[0].src = imageToOpen;
+var btnFav = document.getElementById("btnZoom");
+btnFav.addEventListener("click", addFav, false)
+var counter = window.opener.counterZoom;
+
+var fav1, fav2, fav3, fav4;
+
+
+  function addFav()
+  {
+      counter++;
+
+      switch (counter % 4)
+      {
+          case 1:
+              fav1 = window.opener.document.getElementById("fav1");
+              fav1.innerHTML = '<span onclick="removeFav1()" title="Remove">&Chi;</span><a  title="Full Size" href="' + imageToOpen + '"><img style="width:100px; height:100px;" src=' + imageToOpen + '></a>';
+              //document.getElementById("span1").addEventListener("click", function (){ window.opener.document.getElementById("fav1").innerHTML = ""; }, false);
+              break;
+          case 2:                                                                                            
+              fav2=window.opener.document.getElementById("fav2");
+              fav2.innerHTML = '<span onclick="removeFav2()" title="Remove">&Chi;</span><a  title="Full Size" href="' + imageToOpen + '"><img  style="width:100px; height:100px;"  src=' + imageToOpen + '></a>';
+              break;                                                                                   
+          case 3:                                                                                      
+              fav3=window.opener.document.getElementById("fav3");
+              fav3.innerHTML = '<span onclick="removeFav3()" title="Remove">&Chi;</span><a  title="Full Size" href="' + imageToOpen + '"><img  style="width:100px; height:100px;"  src=' + imageToOpen + '></a>';
+              break;                                                                                         
+          case 0:                                                                                            
+              fav4=window.opener.document.getElementById("fav4");
+              fav4.innerHTML = '<span onclick="removeFav4()" title="Remove">&Chi;</span><a  title="Full Size" href="' + imageToOpen + '"><img  style="width:100px; height:100px;"  src=' + imageToOpen + '></a>';
+              break;
+      }
+      window.opener.counterZoom = counter;
+     
+  }
+
+
